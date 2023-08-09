96bd7dcaa4963bd39358fd680c913a56f3f7d631
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-08-09 00:55:20 -0500

    Install and check how to start with firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 // Este es el punto de entrada de tu aplicacion
 
+import firebase from 'firebase/compat/app';
 import { myFunction } from './lib/index.js';
+// Importar bibliotecas de MongoDB
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const storage = firebase.storage();
+const db = firebase.firestore();
+
+export { auth, storage, db };
 
 myFunction();
