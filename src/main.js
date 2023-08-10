d42039a532cd52a45da55ba132d46c05efdf1f16
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-08-10 02:09:28 -0500

    define structure for routing

--- src/main.js
+++ src/main.js
@@ -1,26 +1,40 @@
 // Este es el punto de entrada de tu aplicacion
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-import 'firebase/compat/storage';
-
 import { myFunction } from './lib/index.js';
+import home from './lib/home';
+import signIn from './lib/signIn';
+import error from './lib/error';
 
-const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+const routes = [
+  { path: '/', component: home },
+  { path: '/signIn', component: signIn },
+  { path: '/error', component: error },
+];
+
+const defaultRoute = '/';
+const root = document.getElementById('root');
+
+function navigateTo(hash) {
+  const route = routes.find((routeFound) => routeFound.path === hash);
+
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+    while (root.firstChild) {
+      root.removeChild(root.firstChild);
+    }
+    root.appendChild(route.component(navigateTo));
+  } else {
+    navigateTo('/error');
+  }
+}
+
+window.onpopstate = () => {
+  navigateTo(window.location.pathname);
 };
 
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const storage = firebase.storage();
-const db = firebase.firestore();
-
-export { auth, storage, db };
+navigateTo(window.location.pathname || defaultRoute);
 
 myFunction();
